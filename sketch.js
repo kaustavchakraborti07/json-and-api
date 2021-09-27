631fa91cd4946fb135a942dffc3b8bc5ca530288
kaustavchakraborti07 <78530875+kaustavchakraborti07@users.noreply.github.com>
2021-09-27 19:34:19 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var backgroundImg;
+
+var bg = "sprites/bg1.png";
+
+function preload() {
+    getBackgroundImg();
+}
+
+function setup(){
+    var canvas = createCanvas(1200,700);
+    engine = Engine.create();
+    world = engine.world;
+
+}
+
+function draw(){
+
+    // add condition to check if any background image is there to add
+    if(backgroundImg)
+    background(backgroundImg);
+
+    Engine.update(engine);
+
+    // write code to display time in correct format here
+
+
+}
+
+async function getBackgroundImg(){
+        var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+        var responseJSON = await response.json();
+    
+        var datetime = responseJSON.datetime;
+        var hour = datetime.slice(11,13);
+        
+        if(hour>=0600 && hour<=1900){
+            bg = "sprites/bg1.png";
+        }
+        else{
+            bg = "sprites/bg2.jpg";
+        }
+    
+        backgroundImg = loadImage(bg);
+        console.log(backgroundImg);
+    }
+   
